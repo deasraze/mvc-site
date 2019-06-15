4a030ecbfc9874dcc1f39cb359d4903b56090d5d
Chopper <ilua292010@gmail.com>
2019-06-15 13:44:27 +0300

    Фиксы прелоадера

--- template/scripts/preloader.js
+++ template/scripts/preloader.js
@@ -1,5 +1,8 @@
 setTimeout(function () {
     $(document).ready(function () {
         $(".loader").css({"transition": "1s","opacity": "0"})
+        setTimeout(function () {
+            $(".loader").css({"display": "none"})
+        },1000)
     });
 },2000);
