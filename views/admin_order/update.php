b8ace5a04b2ca4e632e8bbd2806a84a1dabb46da
satyrian <condecrom@gmail.com>
2019-06-16 20:10:18 +0300

    - fixed errors message output when editing in the admin panel

--- views/admin_order/update.php
+++ views/admin_order/update.php
@@ -166,9 +166,9 @@
             </ol>
         </div>
         <?php if (isset($errors) && is_array($errors)): ?>
-            <ul>
+            <ul class="error-message-two">
                 <?php foreach ($errors as $error): ?>
-                    <li> - <?php echo $error; ?></li>
+                    <li class="list-group-item list-group-item-danger"> - <?php echo $error; ?></li>
                 <?php endforeach; ?>
             </ul>
         <?php endif; ?>
